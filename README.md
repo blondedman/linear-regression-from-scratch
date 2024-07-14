3cd4bfb5c0e12858546863726189370b7103a958
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-07-14 11:59:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# regression-from-scratch
+ 
---
+++ r squared.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from statistics import mean
+import numpy as np
+import random
+
+xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+
+# plt.scatter(xs,ys)
+# plt.show()
+    
+def best_fit_slop_intercept(xs,ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs*ys)) /
+            ((mean(xs)**2) - mean(xs**2))) 
+    b = mean(ys) - m * mean(xs)
+    return m, b
+
+def squared_error(ys_orig,ys_line):
+    return sum((ys_line-ys_orig)**2)
+
+def coeff_of_determination(ys_orig,ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_y_mean_line = squared_error(ys_orig,y_mean_line)
+    squared_error_regression_line = squared_error(ys_orig,ys_line)
+    return 1 - (squared_error_regression_line/squared_error_y_mean_line)
+
+m,b = best_fit_slop_intercept(xs,ys)
+
+print(m,b)
+
+regression_line = [(m*x)+b for x in xs] 
+
+predict_x = 8
+predict_y = m*predict_x + b 
+
+r_squared = coeff_of_determination(ys,regression_line)
+
+print(r_squared)
+
+plt.scatter(xs,ys)
+plt.plot(xs,regression_line)
+plt.scatter(predict_x,predict_y)
+plt.show() 
