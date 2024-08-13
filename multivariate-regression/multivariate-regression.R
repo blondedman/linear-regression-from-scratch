45e1d9aef79bff15d768c4d11cbafcf406b455b7
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-08-13 18:51:22 +0530

    Update multivariate-regression.R

--- multivariate-regression/multivariate-regression.R
+++ multivariate-regression/multivariate-regression.R
@@ -1,6 +1,6 @@
 # resources used: 
 # library.virginia.edu
-# www.datacamp.com
+# cran.r-project.org
 
 # loading the dataset
 data <- read.table("http://static.lib.virginia.edu/statlab/materials/data/ami_data.DAT")
@@ -14,18 +14,11 @@ pairs(data)
 mlm1 <- lm(cbind(TOT, AMI) ~ GEN + AMT + PR + DIAP + QRS, data = data)
 summary(mlm1)
 
-# residual values
-head(resid(mlm1))
-
-# fitted values
 head(fitted(mlm1))
-
-# coefficient values
-coef(mlm1)
-
-# residual standard error 
+head(resid(mlm1))
 sigma(mlm1)
-
-# variance-covariance matrix
+coef(mlm1)
 vcov(mlm1)
 
+library(car)
+Anova(mlm1)
