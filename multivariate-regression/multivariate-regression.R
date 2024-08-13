1944433886aeae7a031b813aef0281909eb03f0a
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-08-13 23:46:28 +0530

    Update multivariate-regression.R

--- multivariate-regression/multivariate-regression.R
+++ multivariate-regression/multivariate-regression.R
@@ -2,6 +2,8 @@
 # library.virginia.edu
 # cran.r-project.org
 
+library(car)
+
 # loading the dataset
 data <- read.table("http://static.lib.virginia.edu/statlab/materials/data/ami_data.DAT")
 names(data) <- c("TOT","AMI","GEN","AMT","PR","DIAP","QRS")
@@ -14,11 +16,14 @@ pairs(data)
 mlm1 <- lm(cbind(TOT, AMI) ~ GEN + AMT + PR + DIAP + QRS, data = data)
 summary(mlm1)
 
-head(fitted(mlm1))
-head(resid(mlm1))
-sigma(mlm1)
-coef(mlm1)
-vcov(mlm1)
-
-library(car)
 Anova(mlm1)
+Manova(mlm1)
+
+# machine learning model 2
+mlm2 <- update(mlm1, . ~ . - PR - DIAP - QRS)
+summary(mlm2)
+
+Anova(mlm2)
+Manova(mlm2)
+
+anova(mlm1, mlm2)
