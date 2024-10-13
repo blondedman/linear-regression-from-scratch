b69502a1d6e77d35ff4aca70d198fb53b797cdab
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-10-13 19:01:13 +0530

    Update multiple-regression-scratch.py

--- multiple-regression/multiple-regression-scratch.py
+++ multiple-regression/multiple-regression-scratch.py
@@ -16,7 +16,18 @@ class multipleregression:
     return (features@self.w) + self.b
 
   def loss(self, groundtruth, predictions):
-    return np.mean(np.square((groundtruth-predictions)))
+    return np.mean(np.square((groundtruth - predictions)))
   
   def gradientdescent(self, features, groundtruth, predictions):
-    pass
+    error = groundtruth - predictions
+    dW = []
+    for column in features.columns:
+      dW.append(-2 * np.mean((error * features[column])))
+    db = -2 * np.mean(error)
+    return dW, db
+  
+  def optimizemodelparametes(self, features, groundtruth, predictions):
+    dW, db = self.gradientdescent(features, groundtruth, predictions)
+    self.weight[0] += self.rate * -dW[0]
+    self.weight[1] += self.rate * -dW[1]
+    self.bias += self.rate * -db
