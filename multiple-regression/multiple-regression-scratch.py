2c01bd51daa80d16880d4114f0731f8c9371f856
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-10-13 10:40:22 +0530

    Update multiple-regression-scratch.py

--- multiple-regression/multiple-regression-scratch.py
+++ multiple-regression/multiple-regression-scratch.py
@@ -5,5 +5,18 @@ def weightplusbias(w, b):
   bias = 0.01 * np.random.rand(b)
   return weights, bias
 
-def MLR(features, weights, bias):
-  return (features@weights) + bias
+class multipleregression:
+  
+  def __init__(self, rate, w, b):
+    self.rate = rate
+    self.w = w
+    self.b = b
+    
+  def MLR(self, features, weights, bias):
+    return (features@self.w) + self.b
+
+  def loss(self, groundtruth, predictions):
+    return np.mean(np.square((groundtruth-predictions)))
+  
+  def gradientdescent(self, features, groundtruth, predictions):
+    pass
